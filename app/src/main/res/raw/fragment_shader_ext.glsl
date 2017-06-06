3a18fb4327e5a16f1026080f4b1e7ad0426e3d53
lansosdk <support@lansongtech.com>
2017-06-06 12:11:59 +0800

    delete build  filedir

--- app/src/main/res/raw/fragment_shader_ext.glsl
+++
@@ -1,9 +0,0 @@
-#extension GL_OES_EGL_image_external : require
-precision mediump float; //指定默认精度
-
-varying vec2 vTextureCoord;
-uniform sampler2D uTexture;
-
-void main() {
-    gl_FragColor = texture2D(uTexture, vTextureCoord);
-}
